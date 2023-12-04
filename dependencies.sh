0824ae8a4ec696d7aa46c95a73d10c56d6b9d16a
Nathan RABET <nathan.rabet@epita.fr>
2023-12-04 19:00:38 +0000

    If OpenSSL < 3.0.0, then install it by yourself :)

--- dependencies.sh
+++ dependencies.sh
@@ -21,7 +21,7 @@ if [ -x "$(command -v apt)" ]; then
     apt update
     apt install -y \
         build-essential make libssl-dev libfuse-dev
-elif [ -x "$(command -v yum)" ]; then
+elif [ -x "$(command -v pacman)" ]; then
     pacman -Syu
     pacman -S --noconfirm \
         base-devel make libssl-dev libfuse-dev
@@ -29,43 +29,6 @@ else
     red "Unsupported OS: Your system must use 'apt' or 'pacman' packages managers"
     exit 1
 fi
-
-manual_openssl_install() {
-    ossl_version="3.2.0"
-    # Ask user if he wants to install OpenSSL $ossl_version
-    read -p "Do you want to install OpenSSL '$ossl_version' (using manual wget, tar and make)? [y/n] " -n 1 -r REPLY
-
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        echo -e "\nExiting..."
-        exit 1
-    fi
-
-    if [ -x "$(command -v apt)" ]; then
-        apt remove -y openssl libssl-dev
-    elif [ -x "$(command -v yum)" ]; then
-        pacman -R openssl libssl-dev
-    fi
-
-    echo "Downloading, configuring, compiling and installing OpenSSL '$ossl_version'"
-    # Creating temporary directory
-    tmp_dir=$(mktemp -d)
-
-    wget https://www.openssl.org/source/openssl-$ossl_version.tar.gz -P $tmp_dir
-    cd $tmp_dir
-    tar -xzf openssl-$ossl_version.tar.gz
-    cd openssl-$ossl_version
-    ./config
-    make -j
-    make -j install
-
-    ldconfig
-    ln -s /usr/local/bin/openssl /usr/bin/
-
-    # Cleaning temporary directory
-    rm -rf $tmp_dir
-
-    green "OpenSSL '$ossl_version' installed successfully"
-}
 
 # Check that openssl version is 3.0.0 or higher
 echo "Checking OpenSSL version..."
@@ -75,13 +38,13 @@ if [ -x "$(command -v openssl)" ]; then
         green "OpenSSL version is '$openssl_version', OK for SherlockFS"
     else
         yellow "OpenSSL version is '$openssl_version', KO for SherlockFS: must be 3.0.0 or higher"
-        manual_openssl_install
+        yellow "You must install OpenSSL 3.0.0 or higher by yourself"
+        exit 1
     fi
 else
     red "OpenSSL not installed"
-    manual_openssl_install
+    exit 1
 fi
 
 green "Dependencies installed successfully!"
-
 exit 0
