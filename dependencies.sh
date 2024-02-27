12390157ffa01c0a0efeaf8e4ae615301a7cb035
Nathan RABET <nathan.rabet@epita.fr>
2024-02-27 19:41:12 +0100

    Fixing GH Actions issue (It was due to an OpenSSL version mismatch)

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+# Check if "-h" or "--help" is passed as an argument
+if [[ "${1}" == "-h" || "${1}" == "--help" ]]; then
+    echo "This script installs the dependencies required to build SherlockFS."
+    echo "If you want to install the dependencies required to run the tests, use the '--with-tests' option."
+    echo
+    echo "Usage: sudo $0 [-h | --help | --with-tests]"
+    exit 0
+fi
+
 # Make sure the script is being executed with superuser privileges.
 if [[ "${UID}" -ne 0 ]]; then
     echo "Please run this script with superuser privileges."
     exit 1
+fi
+
+# Check if "--with-tests" is passed as an argument
+if [[ "${1}" == "--with-tests" ]]; then
+    WITH_TESTS="true"
+else
+    WITH_TESTS="false"
 fi
 
 # Exit on error
@@ -18,13 +34,22 @@ yellow() {
 }
 
 if [ -x "$(command -v apt)" ]; then
+    PACKAGES="build-essential make libssl-dev libfuse-dev openssl"
+    if [ "$WITH_TESTS" = "true" ]; then
+        PACKAGES="$PACKAGES libcriterion-dev"
+    fi
+
     apt update
-    apt install -y \
-        build-essential make libssl-dev libfuse-dev
+    apt install -y $PACKAGES
+
 elif [ -x "$(command -v pacman)" ]; then
+    PACKAGES="base-devel make libssl-dev libfuse-dev openssl"
+    if [ "$WITH_TESTS" = "true" ]; then
+        PACKAGES="$PACKAGES criterion"
+    fi
+
     pacman -Syu
-    pacman -S --noconfirm \
-        base-devel make libssl-dev libfuse-dev
+    pacman -S --noconfirm $PACKAGES
 else
     red "Unsupported OS: Your system must use 'apt' or 'pacman' packages managers"
     exit 1
@@ -32,13 +57,14 @@ fi
 
 # Check that openssl version is 3.0.0 or higher
 echo "Checking OpenSSL version..."
+REQUIRED_OPENSSL_VERSION="3.0.10"
 if [ -x "$(command -v openssl)" ]; then
     openssl_version=$(openssl version | awk '{print $2}')
-    if [ "$(printf '%s\n' "3.0.0" "$openssl_version" | sort -V | head -n1)" = "3.0.0" ]; then
+    if [ "$(printf '%s\n' "$REQUIRED_OPENSSL_VERSION" "$openssl_version" | sort -V | head -n1)" = "$REQUIRED_OPENSSL_VERSION" ]; then
         green "OpenSSL version is '$openssl_version', OK for SherlockFS"
     else
-        yellow "OpenSSL version is '$openssl_version', KO for SherlockFS: must be 3.0.0 or higher"
-        yellow "You must install OpenSSL 3.0.0 or higher by yourself"
+        yellow "OpenSSL version is '$openssl_version', KO for SherlockFS: must be $REQUIRED_OPENSSL_VERSION or higher"
+        yellow "You must install OpenSSL $REQUIRED_OPENSSL_VERSION or higher by yourself"
         exit 1
     fi
 else
