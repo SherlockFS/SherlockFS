eb466837364de9add075238ee4490caebd0f7b44
Clara Chalumeau <clara.chalumeau@epita.fr>
2024-05-05 08:08:20 -0400

    Add more bash test

--- tests/fuse_test.sh
+++ tests/fuse_test.sh
@@ -43,6 +43,16 @@ else
 fi
 }
 
+testEmpty() {
+if [[ -z $1 ]]; then
+    green "[SUCCESS] $2"
+    ((test_success++))
+else
+    red "[ERROR] $2"
+    ((test_failed++))
+fi
+}
+
 blue "\nStarting test ...\n"
 
 cd $MOUNT
@@ -57,6 +67,9 @@ testString $output "file" "Listing file"
 rm file
 test $? 0 "Deleting file"
 
+output=$(ls)
+testEmpty "$output" "Empty file system"
+
 mkdir dir
 test $? 0 "Creation of a directory"
 
@@ -66,13 +79,48 @@ testString $output "dir" "Listing one directory"
 rm -rf dir
 test $? 0 "Deletion of a directory"
 
-echo "This is a test" > file
+echo -n "This is a test" > file
 test $? 0 "Creation and writing to a file"
 
+output=$(cat file)
+test $? 0 "Reading file"
+testString "$output" "This is a test" "Verify content of a file"
+
+echo -n "Append to file" >> file
+test $? 0 "Append to file"
+
+output=$(cat file)
+testString "$output" "This is a testAppend to file" "Verify append the content of a file"
+
+touch
+test $? 1 "Touch without argument"
+
+mkdir symbolic_folder
+cp file symbolic_folder/.
+test $? 0 "Copy file to folder"
+
+output=$(ls symbolic_folder)
+testString "$output" "file" "Listing from directory"
+
+ln -s symbolic_folder/file sym
+test $? 0 "Create symbolic link"
+
+output=$(cat sym)
+test $? 0 "Reading sym link"
+testString "$output" "This is a testAppend to file" "Reading content of sym link"
+
+rm sym
+test $? 0 "Deletion of sym link"
 rm file
 
-green "\nNumber of test successful : $test_success"
-red "Number of test failed : $test_failed"
+output=$(ls)
+testString "$output" "symbolic_folder" "Deletion of sym link and remaining folder"
+
+rm -rf symbolic_folder
+
+
+blue "\nNumber of test successful : $test_success"
+blue "Number of test failed : $test_failed"
 
 cd ..
 
